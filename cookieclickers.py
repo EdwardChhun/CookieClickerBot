5d6e8661b5a2e4f3303472b68bd31fc4850f170d
Edward Chhun <edwardchhun3@gmail.com>
2024-05-16 02:08:41 -0700

    Added Files

---
+++ cookieclickers.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+options = Options()
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+
+cookie_id = "bigCookie"
+cookies_id = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
+
+WebDriverWait(driver,5).until(
+    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
+)
+
+language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
+language.click()
+
+WebDriverWait(driver,5).until(
+    EC.presence_of_element_located((By.ID, cookie_id))
+)
+
+cookie = driver.find_element(By.ID, cookie_id)
+
+while True:
+    cookie.click()
+    cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
+    cookies_count = int(cookies_count.replace(",", ""))
+    print(cookies_count)
+    
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+        
+        if not product_price.isdigit():
+            continue
+        
+        product_price = int(product_price)
+
+        if cookies_count >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
+        
+time.sleep(10)
---
+++ main.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+options = Options()
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://google.com")
+#-------------------------------------------------
+# From here above is the boiler plate for selenium
+
+# Waiting for the element to exist
+WebDriverWait(driver,5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
+# Finding then element and then do inputs
+input_element = driver.find_element(By.CLASS_NAME,"gLFyf")
+input_element.clear()
+input_element.send_keys("Sacramento City College" + Keys.ENTER)
+
+# Test if links exist
+WebDriverWait(driver,5).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Sacramento City College"))
+)
+
+# Finding the partial link and click on it
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Sacramento City College")
+
+link.click()
+
+time.sleep(10)
+
+driver.quit()
